129df63591045b8514b85856396d3ffedf77aaad
Gajesh Bhat <16153045+gajeshbhat@users.noreply.github.com>
2021-01-02 23:58:17 -0500

    Added NLTK NB features

--- app.py
+++ app.py
@@ -1,16 +1,34 @@
+import pickle
 from flask import Flask
 from flask_restful import Resource, Api
+from nltk.tokenize import word_tokenize
 
 app = Flask(__name__)
 api = Api(app)
 
+# Twitter Sentiment NLTK Naive Bayes
+nb_senti_file = open('models/tweet_senti_nb.pickle', 'rb')
+nb_senti_classifier = pickle.load(nb_senti_file)
+nb_senti_file.close()
 
-class HelloWorld(Resource):
+
+class SentimentNLTKNB(Resource):
     def get(self):
-        return {'hello': 'world'}
+        return {'sentiment': str(self.get_sentiment())}
+
+    def get_sentiment(self,tweet):
+        clean_tweet = self.get_tokenized_tweet(tweet)
+        tokenized_tweet = self.get_tokenized_tweet(clean_tweet)
+        return nb_senti_classifier.classify(tokenized_tweet)
+
+    def get_nltk_tokens(self, cleaned_tokens):
+        return dict([token, True] for token in cleaned_tokens)
+
+    def get_tokenized_tweet(self, tweet):
+        return word_tokenize(tweet)
 
 
-api.add_resource(HelloWorld, '/')
+api.add_resource(SentimentNLTKNB, '/')
 
 if __name__ == '__main__':
     app.run(debug=True)
