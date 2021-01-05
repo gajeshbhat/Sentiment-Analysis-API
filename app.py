24abfe5dbe3e5ed7c1b50570a3e2ecbcfc10ed17
Gajesh Bhat <16153045+gajeshbhat@users.noreply.github.com>
2021-01-04 21:58:21 -0500

    Added Flair sentiment analyzer

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import pickle
+import flair
+import stanza
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
 from models.nltk_sentiment_nb import remove_noise, download_packages, stopwords, word_tokenize
@@ -20,6 +22,12 @@ parser.add_argument('data', required=True, help="Need tweet data to perform sent
 nb_senti_file = open('models/tweet_senti_nb.pickle', 'rb')
 nb_senti_classifier = pickle.load(nb_senti_file)
 nb_senti_file.close()
+
+# Flair load pretrained model
+flair_pre_model = flair.models.TextClassifier.load('en-sentiment')
+
+# Standford CORE NLP Stanza Sentiment Pipeline
+stan_pipeline = stanza.Pipeline(lang='en', processors='tokenize,mwt,pos,lemma,sentiment')
 
 
 class SentimentNLTKNB(Resource):
@@ -67,9 +75,29 @@ class SentimentTextBlob(Resource):
         return get_sentiment_details(tweet)
 
 
+class SentimentFlair(Resource):
+    def post(self):
+        args = parser.parse_args()
+        tweet_data = str(args['data'])
+        return {'tweet': str(tweet_data), 'sentiment_scores': self.get_sentiment(tweet_data, flair_pre_model)}
+
+    def get_cleaned_tweet(self, modelled_tweet):
+        sentiment = modelled_tweet.labels
+        sentiment_data_list = str(sentiment[0]).split()
+        sentiment_data_dict = {'overall_sentiment': sentiment_data_list[0], 'polarity_score': sentiment_data_list[1]}
+        return sentiment_data_dict
+
+    def get_sentiment(self, tweet, model):
+        modelled_tweet = flair.data.Sentence(tweet)
+        model.predict(modelled_tweet)
+        cleaned_sentiment_data = self.get_cleaned_tweet(modelled_tweet)
+        return cleaned_sentiment_data
+
+
 api.add_resource(SentimentNLTKNB, '/sentiment/nb')
 api.add_resource(SentimentNLTKVader, '/sentiment/vader')
 api.add_resource(SentimentTextBlob, '/sentiment/tb')
+api.add_resource(SentimentFlair, '/sentiment/fl')
 
 if __name__ == '__main__':
     app.run(debug=True)
